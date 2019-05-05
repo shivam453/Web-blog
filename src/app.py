3ac2d643d41cce482894ca9d8d22ad8aaa6cfd06
Shivam <goelshivam453@gmail.com>
2019-05-05 06:00:18 +0530

    bootstrap

--- src/app.py
+++ src/app.py
@@ -6,14 +6,22 @@ app = Flask(__name__)
 app.secret_key = 'shivam'
 
 @app.route('/')
-def hello_method():
+def home_template():
+    return render_template('home.html')
+
+@app.route('/login')
+def login_template():
     return render_template('login.html')
+
+@app.route('/register')
+def register_template():
+    return render_template('register.html')
 
 @app.before_first_request
 def initialize_database():
     Database.initialze()
 
-@app.route('/login', methods=['POST'])
+@app.route('/auth/login', methods=['POST'])
 def login_user():
     email = request.form['email']
     password = request.form['password']
@@ -23,6 +31,15 @@ def login_user():
     else:
         session['email'] = None
     
+    return render_template('profile.html', email=session['email'])
+
+@app.route('/auth/register', methods=['POST'])
+def register_user():
+    email = request.form['email']
+    password = request.form['password']
+
+    User.register(email, password)
+
     return render_template('profile.html', email=session['email'])
 
 if __name__== '__main__':
